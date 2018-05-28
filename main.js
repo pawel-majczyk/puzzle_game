b70bedc762f8944839562732a96db921d6e28780
Paweł Majczyk <pawel.majczyk@gmail.com>
2018-05-28 10:34:05 +0200

    refactor

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ class Gameboard {
         this.maxScore = Math.pow(size, 2);
         this.boardWidth = boardWidth;
         this.puzzles = [];
-
-        this.gamearea = document.getElementById('gamearea');
+        this.gameApp = document.getElementById('game_app');
         this.gameboard = document.createElement('div');
-
-        this.bgimg = './img/puzzle_gray.png'; //temporary hardcoded
-        this.gameboard.style.background = `url(${this.bgimg})`;
         this.gameboard.setAttribute('id', 'gameboard');
+        this.gameApp.appendChild(this.gameboard);
+        this.createPuzzles();
+        this.setBackground();
+    }
+    static getPicture() {
+        if (!this.bgimage) {
+            this.bgimage = 'https://source.unsplash.com/random/512x512';
+            return this.bgimage;
+        } else {
+            return this.bgimage;
+        }
+    }
 
-        this.gamearea.appendChild(this.gameboard);
+    setBackground() {
+        this.bgimage = 'https://source.unsplash.com/random/512x512';
+        // this.gameboard.style.background = `url(${this.bgimage})`; //to-do with filter
     }
 
     createPuzzles() {
@@ -34,7 +44,6 @@ class Gameboard {
             }
             let puzzle = new Puzzle(id, posX, posY, currentPosX, currentPosY, puzzleSize);
             this.puzzles.push(puzzle);
-            console.log(this.puzzles[i].posX, this.puzzles[i].posY); //eslint-disable-line
         }
         this.puzzles.forEach(puzzle => {
             this.gameboard.appendChild(puzzle.puzzleDiv);
@@ -43,23 +52,18 @@ class Gameboard {
 
     checkScore() {
         if (this.score >= this.maxScore) {
-            gameboard.style.border = '3px solid limegreen';
             setTimeout(() => {
-                alert('Gratuluję! Jesteś zwycienscom!!!1111');
+                alert('Gratuluję! Wygrałeś(aś)!');
             }, 250);
-            // setTimeout(() => {
-            //     window.location.replace('https://www.youtube.com/watch?v=1Bix44C1EzY');
-            // }, 1000);
         }
     }
 }
 
 class Puzzle {
     constructor(id, posX, posY, currentPosX, currentPosY, puzzleSize) {
-        const initialOffset = 50;
+        const initialOffset = 25;
         this.posX = posX;
         this.posY = posY;
-        this.bgimg = './img/puzzle_org.png'; //temporary hardcoded
         this.currentPosX = currentPosX;
         this.currentPosY = currentPosY + initialOffset;
         this.locked = false;
@@ -69,103 +73,99 @@ class Puzzle {
         puzzleDiv.id = id;
         puzzleDiv.style.left = `${this.currentPosX}px`;
         puzzleDiv.style.top = `${this.currentPosY}px`;
-        puzzleDiv.style.background = `url(${this.bgimg})`;
+        puzzleDiv.style.background = `url(${Gameboard.getPicture()})`;
         puzzleDiv.style.backgroundPositionX = `${posX}px`;
         puzzleDiv.style.backgroundPositionY = `${posY}px`;
         puzzleDiv.style.width = `${puzzleSize}px`;
         puzzleDiv.style.height = `${puzzleSize}px`;
-        puzzleDiv.style.border = '1px dotted yellow';
+        puzzleDiv.addEventListener('mousedown', this.startDragging);
+        puzzleDiv.addEventListener('mousemove', this.whileDragging);
+        puzzleDiv.addEventListener('mouseup', this.stopDragging);
+
         this.puzzleDiv = puzzleDiv;
     }
+
+    startDragging(e) {
+        this.classList.add('--dragged');
+        Puzzle.checkForLock(e);
+        if (!this.locked) {
+            this.draggabble = true;
+        }
+    }
+
+    whileDragging(e) {
+        if (this.draggabble) {
+            this.style.top = e.clientY - 128 + 'px';
+            this.style.left = e.clientX - 128 + 'px';
+            let fits = Puzzle.checkForLock(e);
+            if (fits) {
+                this.classList.add('--it-fits');
+            } else {
+                this.classList.remove('--it-fits');
+            }
+        }
+        // showCursorPos(e); //dev utility
+    }
+
+    stopDragging(e) {
+        this.removeEventListener('mousemove', this.whileDragging);
+        this.classList.remove('--dragged', '--it-fits');
+        this.draggabble = false;
+        if (Puzzle.checkForLock(e)) {
+            this.classList.add('--fitted');
+            this.style.top = Puzzle.getRefPoints(this.id).top + 'px';
+            this.style.left = Puzzle.getRefPoints(this.id).left + 'px';
+            if (!this.locked) {
+                game.score += 1;
+                game.checkScore();
+            }
+            this.locked = true;
+        }
+    }
+
+    static checkForLock(e) {
+        let correctSpot = false;
+        const refPoints = Puzzle.getRefPoints(e.target.id);
+        const snapRange = 18;
+        const elementIsWithinDropZone =
+            (refPoints.left - e.target.offsetLeft <= snapRange &&
+                refPoints.left - e.target.offsetLeft >= -snapRange) &&
+            (refPoints.top - e.target.offsetTop <= snapRange &&
+                refPoints.top - e.target.offsetTop >= -snapRange);
+
+        if (elementIsWithinDropZone) {
+            correctSpot = true;
+        } else {
+            correctSpot = false;
+        }
+        return correctSpot;
+    }
+
+    static getRefPoints(targetId) {
+        let references = {};
+        references.top = gameboard.offsetTop + game.puzzles[targetId].posY;
+        references.left = gameboard.offsetLeft + game.puzzles[targetId].posX;
+        return references;
+    }
+
+
 }
 
 const game = new Gameboard(2);
-game.createPuzzles();
-
 const gameboard = document.querySelector('#gameboard');
-const puzzles = document.querySelectorAll('.game__puzzle');
-const headings = document.getElementsByTagName('h1');
 
-puzzles.forEach(puzzle => {
-    puzzle.addEventListener('mousedown', startDragging);
-});
+//developer utils
+// const headings = document.getElementsByTagName('h1');
+// function showCursorPos(e) {
+//     headings[0].innerHTML = `.${e.target.className}#${e.target.id} <br>
+//     e.clientX = ${e.clientX} e.clientY = ${e.clientY};<br>
+//      cursor pos: X:${e.offsetX} Y:${e.offsetY} ||${e.target.offsetLeft+e.offsetX}:${e.target.offsetTop+e.offsetY} <br>
+//     posX:${e.target.offsetLeft}/posY:${e.target.offsetTop}<br>
+//     calc:<br>
+//     gameboard.offsetLeft ${gameboard.offsetLeft} <br>
+//     gameboard.offsetTop ${gameboard.offsetTop} <br>
+//     e.target.offsetLeft  ${e.target.offsetLeft}<br>
+//     e.target.offsetTop  ${e.target.offsetTop}<br>
 
-// let beingDragged = false;
-let correctSpot = false;
-
-function startDragging(e) {
-    e.preventDefault();
-    this.style.zIndex = '100'; //move on top, so other puzzles do not block moving
-    this.addEventListener('mousemove', whileDragging);
-}
-
-function whileDragging(e) {
-    this.style.opacity = 0.5;
-    this.style.top = e.clientY - 128 + 'px';
-    this.style.left = e.clientX - 128 + 'px';
-    showCursorPos(e);
-    checkForLock(e);
-    this.addEventListener('mouseup', stopDragging);
-}
-
-function stopDragging() {
-    this.removeEventListener('mousemove', whileDragging);
-    this.style.opacity = 1;
-    this.style.zIndex = '10';
-    if (correctSpot) {
-        this.style.border = 'none';
-        this.style.top = getRefPoints(this.id).top + 'px';
-        this.style.left = getRefPoints(this.id).left + 'px';
-        this.style.zIndex = 10;
-        this.removeEventListener('mousedown', startDragging);
-        if (!this.locked) {
-            game.score += 1;
-            game.checkScore();
-        };
-        this.locked = true;
-    } else {
-        this.style.border = '1px dotted yellow';
-    }
-}
-
-
-function checkForLock(e) {
-    const refPoints = getRefPoints(e.target.id);
-    const snapRange = 10;
-    const elementIsWithinDropZone = (
-        refPoints.left - e.target.offsetLeft <= snapRange &&
-        refPoints.left - e.target.offsetLeft >= -snapRange)
-     && (refPoints.top - e.target.offsetTop <= snapRange &&
-        refPoints.top - e.target.offsetTop >= -snapRange);
-
-    if (elementIsWithinDropZone) {
-        e.target.style.border = '2px solid limegreen';
-        correctSpot = true;
-    } else {
-        e.target.style.border = '1px solid orangered';
-        correctSpot = false;
-    }
-}
-
-function getRefPoints(targetId) {
-    let references = {};
-    references.top = gameboard.offsetTop + game.puzzles[targetId].posY;
-    references.left = gameboard.offsetLeft + game.puzzles[targetId].posX;
-    return references;
-}
-
-
-
-function showCursorPos(e) {
-    headings[0].innerHTML = `.${e.target.className}#${e.target.id} <br>
-    e.clientX = ${e.clientX} e.clientY = ${e.clientY};<br>
-     cursor pos: X:${e.offsetX} Y:${e.offsetY} ||${e.target.offsetLeft+e.offsetX}:${e.target.offsetTop+e.offsetY} <br>
-    posX:${e.target.offsetLeft}/posY:${e.target.offsetTop}<br>
-    calc:<br>
-    gameboard.offsetLeft ${gameboard.offsetLeft} <br>
-    gameboard.offsetTop ${gameboard.offsetTop} <br>
-    e.target.offsetLeft  ${e.target.offsetLeft}<br>
-    e.target.offsetTop  ${e.target.offsetTop}<br>
-    
-    `;
-}
+//     `;
+// }
